626077750ef7161e5e2209c3a20dd664a566f606
kkg5 <kkg05102@gmail.com>
2023-04-16 23:17:49 +0900

    feat : 사용자 정보 가져오기 + pykiwoom 연동
    Co-authored-by: Hong SeungJae <Grooomit@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtCore import Qt
 from PySide2.QtAxContainer import QAxWidget
+from pykiwoom.kiwoom import Kiwoom
 from view import Ui_Dialog
 
 
@@ -8,11 +10,47 @@ class MainWindow(QMainWindow, Ui_Dialog, QAxWidget):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
 
-        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.kiwoom = Kiwoom()
+
+        # self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
         self.loginButton.clicked.connect(self.login)
+        self.kiwoom.ocx.OnEventConnect.connect(self.event_connect)
+        self.pwdButton.clicked.connect(self.getAccountData)
+
+    def event_connect(self, err_code):
+        if err_code == 0:
+            print("로그인 성공")
+
+            name = self.kiwoom.GetLoginInfo("USER_NAME")
+
+            self.loginLabel.setAlignment(Qt.AlignRight)
+            self.loginLabel.setText(f'{name}님 반갑습니다.')
+
+            self.loginButton.hide()
 
     def login(self):
-        return self.kiwoom.dynamicCall("CommConnect()")
+        kiwoom = self.kiwoom
+
+        self.loginButton.setDisabled(True)
+        kiwoom.CommConnect(block=True)
+        self.loginButton.setDisabled(False)
+
+        account_numbers = kiwoom.GetLoginInfo("ACCNO")
+        self.accountList.addItems(account_numbers)
+
+    def getAccountData(self):
+        kiwoom = self.kiwoom
+        print(self.accountList.currentText())
+        print(len(self.pwdInput.text()))
+        kiwoom.SetInputValue("계좌번호", self.accountList.currentText())
+        # kiwoom.SetInputValue("비밀번호", self.pwdInput.text())
+        kiwoom.SetInputValue("비밀번호입력매체구분", "00")
+        kiwoom.SetInputValue("조회구분", "1")
+        kiwoom.CommRqData("opw00004_req", "opw00004", 0, "2000")
+
+        # 예수금 정보 출력하기
+        deposit = kiwoom.GetCommData("opw00004", "", "d+2추정예수금", 0)
+        print(f"예수금: {deposit}")
 
 
 if __name__ == "__main__":
--- view.py
+++ view.py
@@ -1,22 +1,16 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-# Form generated from reading UI file 'gobillionairejpMPkk.ui'
+# Form generated from reading UI file 'gobillionaireCOsWCI.ui'
 ##
-# Created by: Qt User Interface Compiler version 6.2.4
+# Created by: Qt User Interface Compiler version 5.15.2
 ##
 # WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide2.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
-                            QMetaObject, QObject, QPoint, QRect,
-                            QSize, QTime, QUrl, Qt)
-from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-                           QFont, QFontDatabase, QGradient, QIcon,
-                           QImage, QKeySequence, QLinearGradient, QPainter,
-                           QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide2.QtWidgets import (QApplication, QDialog, QPushButton, QSizePolicy,
-                               QTabWidget, QWidget)
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
 
 
 class Ui_Dialog(object):
@@ -26,7 +20,7 @@ class Ui_Dialog(object):
         Dialog.resize(640, 480)
         self.tabWidget = QTabWidget(Dialog)
         self.tabWidget.setObjectName(u"tabWidget")
-        self.tabWidget.setGeometry(QRect(10, 10, 621, 461))
+        self.tabWidget.setGeometry(QRect(10, 20, 621, 451))
         self.tabWidget.setContextMenuPolicy(Qt.DefaultContextMenu)
         self.tabWidget.setLayoutDirection(Qt.LeftToRight)
         self.strategy_tab = QWidget()
@@ -34,17 +28,44 @@ class Ui_Dialog(object):
         self.tabWidget.addTab(self.strategy_tab, "")
         self.asset_tab = QWidget()
         self.asset_tab.setObjectName(u"asset_tab")
+        self.accountList = QComboBox(self.asset_tab)
+        self.accountList.setObjectName(u"accountList")
+        self.accountList.setEnabled(True)
+        self.accountList.setGeometry(QRect(20, 20, 161, 22))
+        self.pwdInput = QLineEdit(self.asset_tab)
+        self.pwdInput.setObjectName(u"pwdInput")
+        self.pwdInput.setGeometry(QRect(190, 20, 61, 20))
+        self.pwdInput.setInputMask(u"")
+        self.pwdInput.setValidator(QIntValidator())  # 숫자만 입력할 수 있도록 설정
+        self.pwdInput.setMaxLength(4)
+        self.pwdInput.setFrame(True)
+        self.pwdInput.setEchoMode(QLineEdit.Password)
+        self.pwdButton = QPushButton(self.asset_tab)
+        self.pwdButton.setObjectName(u"pwdButton")
+        self.pwdButton.setGeometry(QRect(260, 20, 31, 21))
+        self.label = QLabel(self.asset_tab)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(310, 25, 56, 12))
+        self.assetView = QTableView(self.asset_tab)
+        self.assetView.setObjectName(u"assetView")
+        self.assetView.setGeometry(QRect(20, 60, 571, 341))
         self.tabWidget.addTab(self.asset_tab, "")
         self.auto_tab = QWidget()
         self.auto_tab.setObjectName(u"auto_tab")
-        self.loginButton = QPushButton(self.auto_tab)
-        self.loginButton.setObjectName(u"loginButton")
-        self.loginButton.setGeometry(QRect(530, 10, 75, 23))
         self.tabWidget.addTab(self.auto_tab, "")
+        self.loginButton = QPushButton(Dialog)
+        self.loginButton.setObjectName(u"loginButton")
+        self.loginButton.setGeometry(QRect(550, 10, 75, 23))
+        self.loginLabel = QLabel(Dialog)
+        self.loginLabel.setObjectName(u"loginLabel")
+        self.loginLabel.setGeometry(QRect(460, 15, 161, 20))
+        self.loginLabel.raise_()
+        self.tabWidget.raise_()
+        self.loginButton.raise_()
 
         self.retranslateUi(Dialog)
 
-        self.tabWidget.setCurrentIndex(2)
+        self.tabWidget.setCurrentIndex(1)
 
         QMetaObject.connectSlotsByName(Dialog)
     # setupUi
@@ -54,10 +75,17 @@ class Ui_Dialog(object):
             QCoreApplication.translate("Dialog", u"Dialog", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(
             self.strategy_tab), QCoreApplication.translate("Dialog", u"\uc804\ub7b5\uc870\ud68c", None))
+        self.pwdInput.setPlaceholderText(QCoreApplication.translate(
+            "Dialog", u"\ube44\ubc00\ubc88\ud638", None))
+        self.pwdButton.setText(
+            QCoreApplication.translate("Dialog", u"OK", None))
+        self.label.setText(QCoreApplication.translate(
+            "Dialog", u"\ubcf4\uc720 \uae08\uc561: ", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(
             self.asset_tab), QCoreApplication.translate("Dialog", u"\uc790\uc0b0\uc870\ud68c", None))
-        self.loginButton.setText(QCoreApplication.translate(
-            "Dialog", u"\ud0a4\uc6c0 \ub85c\uadf8\uc778", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(
             self.auto_tab), QCoreApplication.translate("Dialog", u"\uc790\ub3d9\ub9e4\ub9e4", None))
+        self.loginButton.setText(QCoreApplication.translate(
+            "Dialog", u"\ud0a4\uc6c0 \ub85c\uadf8\uc778", None))
+        self.loginLabel.setText("")
     # retranslateUi
